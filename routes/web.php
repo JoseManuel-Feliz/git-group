07f40e2139c3a1e9383a468a771674a99f848ed6
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-11-11 12:49:38 +0100

    created routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\PostController as AdminPostController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware("auth")->prefix("/admin")->name("admin.")->group(function() {
+    Route::get('/posts', [AdminPostController::class, 'index'])->name('posts.index');
+    Route::post('/posts', [AdminPostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/create', [AdminPostController::class, 'create'])->name('posts.create');
+    Route::get('/posts/{id}', [AdminPostController::class, 'show'])->name('posts.show');
+    Route::put('/posts/{id}', [AdminPostController::class, "update"])->name("posts.update");
+    Route::delete('/posts/{id}', [AdminPostController::class, "destroy"])->name("posts.delete");
+    Route::get('/posts/{id}/edit', [AdminPostController::class, "edit"])->name("posts.edit");
+});
